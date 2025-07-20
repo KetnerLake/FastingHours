18f799c34aaab27ede393857860aba1001a04c4e
Kevin Hoyt <parkerkrhoyt@gmail.com>
2025-07-20 17:30:09 -0600

    Added PWA manifest
    Also associated icons.

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -7,10 +7,10 @@ function __memo(fn) {
 return {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set(["favicon.svg"]),
-	mimeTypes: {".svg":"image/svg+xml"},
+	assets: new Set(["favicon.svg","fh.svg","icon512_maskable.png","icon512_rounded.png","manifest.json"]),
+	mimeTypes: {".svg":"image/svg+xml",".png":"image/png",".json":"application/json"},
 	_: {
-		client: {start:"_app/immutable/entry/start.C8epUSe7.js",app:"_app/immutable/entry/app.C6Yc10xQ.js",imports:["_app/immutable/entry/start.C8epUSe7.js","_app/immutable/chunks/DM-PvHDN.js","_app/immutable/chunks/oT2D85Fd.js","_app/immutable/chunks/CuFKNimY.js","_app/immutable/entry/app.C6Yc10xQ.js","_app/immutable/chunks/CuFKNimY.js","_app/immutable/chunks/oT2D85Fd.js","_app/immutable/chunks/CWj6FrbW.js","_app/immutable/chunks/C7XsMuZO.js","_app/immutable/chunks/Dw0E50vo.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.Zy1KXJbP.js",app:"_app/immutable/entry/app.WQv79sxD.js",imports:["_app/immutable/entry/start.Zy1KXJbP.js","_app/immutable/chunks/eclHV-qt.js","_app/immutable/chunks/oT2D85Fd.js","_app/immutable/chunks/CuFKNimY.js","_app/immutable/entry/app.WQv79sxD.js","_app/immutable/chunks/CuFKNimY.js","_app/immutable/chunks/oT2D85Fd.js","_app/immutable/chunks/CWj6FrbW.js","_app/immutable/chunks/C7XsMuZO.js","_app/immutable/chunks/Dw0E50vo.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.C-h-JD8K.js","_app/immutable/chunks/CWj6FrbW.js","_app/immutable/chunks/CuFKNimY.js","_app/immutable/chunks/oT2D85Fd.js","_app/immutable/chunks/DM-PvHDN.js"];
+export const imports = ["_app/immutable/nodes/1.DsUiihYf.js","_app/immutable/chunks/CWj6FrbW.js","_app/immutable/chunks/CuFKNimY.js","_app/immutable/chunks/oT2D85Fd.js","_app/immutable/chunks/eclHV-qt.js"];
 export const stylesheets = [];
 export const fonts = [];
