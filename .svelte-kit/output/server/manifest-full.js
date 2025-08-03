c0470a62ba6d549bc82f7b67428b9125a898b4fc
Kevin Hoyt <parkerkrhoyt@gmail.com>
2025-08-03 15:21:24 -0600

    Adding ads.txt file
    Suggested by Google.

--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -7,10 +7,10 @@ function __memo(fn) {
 return {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set(["favicon.svg","fh.svg","icon512_maskable.png","icon512_rounded.png","manifest.json"]),
-	mimeTypes: {".svg":"image/svg+xml",".png":"image/png",".json":"application/json"},
+	assets: new Set(["ads.txt","favicon.svg","fh.svg","icon512_maskable.png","icon512_rounded.png","manifest.json"]),
+	mimeTypes: {".txt":"text/plain",".svg":"image/svg+xml",".png":"image/png",".json":"application/json"},
 	_: {
-		client: {start:"_app/immutable/entry/start.Wt_5WkrQ.js",app:"_app/immutable/entry/app.CCVbyWNp.js",imports:["_app/immutable/entry/start.Wt_5WkrQ.js","_app/immutable/chunks/OEiBBBpp.js","_app/immutable/chunks/BBZTakrf.js","_app/immutable/chunks/JrvXgJed.js","_app/immutable/entry/app.CCVbyWNp.js","_app/immutable/chunks/JrvXgJed.js","_app/immutable/chunks/BBZTakrf.js","_app/immutable/chunks/CWj6FrbW.js","_app/immutable/chunks/BpwAfYwA.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.DgJNncYK.js",app:"_app/immutable/entry/app.C-GPLdVt.js",imports:["_app/immutable/entry/start.DgJNncYK.js","_app/immutable/chunks/DJZjqmDE.js","_app/immutable/chunks/BBZTakrf.js","_app/immutable/chunks/JrvXgJed.js","_app/immutable/entry/app.C-GPLdVt.js","_app/immutable/chunks/JrvXgJed.js","_app/immutable/chunks/BBZTakrf.js","_app/immutable/chunks/CWj6FrbW.js","_app/immutable/chunks/BpwAfYwA.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DthVls-j.js","_app/immutable/chunks/CWj6FrbW.js","_app/immutable/chunks/DrWP1jWg.js","_app/immutable/chunks/JrvXgJed.js","_app/immutable/chunks/BBZTakrf.js","_app/immutable/chunks/OEiBBBpp.js"];
+export const imports = ["_app/immutable/nodes/1.5lwQcTF-.js","_app/immutable/chunks/CWj6FrbW.js","_app/immutable/chunks/DrWP1jWg.js","_app/immutable/chunks/JrvXgJed.js","_app/immutable/chunks/BBZTakrf.js","_app/immutable/chunks/DJZjqmDE.js"];
 export const stylesheets = [];
 export const fonts = [];
